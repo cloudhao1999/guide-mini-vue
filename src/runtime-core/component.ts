b26943383ddc78d4a54ed7d9f210d4bf3d4c798d
chen.yunhao <cyh19991002@gmail.com>
2023-02-13 15:40:36 +0800

    27.实现组件 emit 功能

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,4 +1,5 @@
 import { shallowReadonly } from "../reactivity/reactive";
+import { emit } from "./componentEmit";
 import { initProps } from "./componentProps";
 import { publicInstanceProxyHandlers } from "./componentPublicInstance";
 
@@ -8,7 +9,10 @@ export function createComponentInstance(vnode) {
         type: vnode.type,
         setupState: {},
         props: {},
+        emit: () => {},
     }
+
+    component.emit = emit.bind(null, component) as any;
 
     return component;
 }
@@ -34,7 +38,9 @@ function setupStatefulComponent(instance) {
     const { setup } = Component;
 
     if (setup) {
-        const setupResult = setup(shallowReadonly(instance.props));
+        const setupResult = setup(shallowReadonly(instance.props), {
+            emit: instance.emit
+        });
 
         handleSetupResult(instance, setupResult);
     }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -7,3 +7,15 @@ export const hasChanged = (val, newValue) => {
 }
 
 export const hasOwn = (target, key) => Object.prototype.hasOwnProperty.call(target, key);
+
+export const camelize = (str: string) => {
+    return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ""));
+}
+
+export const capitalize = (str: string) =>  {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export const toHandlerKey = (str: string) => {
+    return str ? `on${capitalize(str)}` : "";
+}
